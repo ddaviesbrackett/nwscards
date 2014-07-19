89cbb6d44c758eeb2f746022366ed54ffb3bab81
Dan Davies Brackett <dan@daviesbrackett.com>
2014-07-19 14:07:30 -0700

    uncheck class checkboxes when selecting school radio

--- public/script/new.js
+++ public/script/new.js
@@ -1,5 +1,12 @@
 $(function(){
 	$('input:radio[name="indiv-class"]').on('click', function(ev){
-		$('.form-group.individual-classes').fadeToggle(400);
+		var $classes = $('.form-group.individual-classes');
+		if(this.id == 'indiv-class-classes') {
+			$classes.fadeIn(400);
+		}
+		else {
+			$classes.fadeOut(400);
+			$classes.find('input[type="checkbox"]').attr('checked',false);
+		}
 	});
 });
