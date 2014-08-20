e0bfaf15e7ce5f1a8c682024fecfbc061f163fab
Dan Davies Brackett <dan@daviesbrackett.com>
2014-08-19 21:52:43 -0700

    staging should be in debug mode

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,8 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
-
+	'local' => array('homestead', 'box903.bluehost.com', 'nwscards.daviesbrackett.com'),
+	
 ));
 
 /*
