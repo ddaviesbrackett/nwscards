d860d659ff9f64c460fb6b324fe494c5c30dd47c
Dan Davies Brackett <dan@daviesbrackett.com>
2014-08-21 20:23:24 -0700

    make debit agreement a partial

---
+++ app/views/partial/debitterms.blade.php
@@ -0,0 +1,49 @@
+		<p>
+       	I authorize the Nelson Waldorf School Parent Association (hereinafter “PAC”) to make pre-authorized debits (PAD) from my account with the aforementioned 
+       	financial institution, either:
+       	<ul>
+	       	<li>bi-weekly, commencing on {{{$dates['charge']['biweekly']}}} and ending on June 3, 2015</li>
+	       	<li>monthly, commencing on {{{$dates['charge']['monthly']}}} and ending on June 3, 2015</li>
+       	</ul>
+       	as selected on the order form.
+       	<p>
+       	Each withdrawal will correspond to a fixed amount equal to the total stated on the order form.  
+       	I understand my account will be debited two business days before the published delivery dates for my order frequency. 
+       	</p>
+       	</p>
+       	<h4>WAIVER</h4>  
+       	<p>
+       	I hereby waive any and all requirements for pre-notification of debiting.  
+       	I will receive a copy of this Agreement by email and waive all other confirmation before the first payment.  
+       	</p>
+       	<h4>SIGNATURES</h4>
+       	<b>I guarantee that all persons whose signatures are required for this account have authorized this agreement.</b>
+
+		<h4>CHANGE AND/OR CANCELLATION: </h4>
+
+		<p>I will advise PAC of any changes to this Agreement including, without limitation, changes to my account number, 
+		  by sending a written notice to PAC at grocerycards@nelsonwaldorf.org at least 10 business days prior to the next withdrawal. 
+		I retain the right to revoke my authorization at any time, by sending a written notice to PAC at grocerycards@nelsonwaldorf.org 
+		  at least 10 business days prior to the next withdrawal. 
+		To obtain a sample of the cancellation form or for more information on my right to cancel a PAD Agreement, 
+		I may contact my financial institution or consult the Canadian Payments Association at <a href="http://www.cdnpay.ca">www.cdnpay.ca</a>. 
+		I agree to release the financial institution of any liability if the revocation is not respected, except in the case of gross negligence on its part. 
+		I agree that the financial institution at which I maintain the account is not required to verify that the payment is debited in accordance with this authorization. 
+		  I also certify that every person whose signature is required for the operation of the aforementioned account has signed this authorization.  
+		I acknowledge that the delivery of this authorization to PAC constitutes delivery by me to the aforementioned financial institution.  
+		</p>
+		<h4>REIMBURSEMENT</h4>
+		<p>                
+		I have certain rights of recourse if a debit does not comply with the terms of this Agreement. For example, I have the right to receive reimbursement 
+		for any PAD that is not authorized or that is not compatible with the terms of this PAD Agreement. For more information on my rights of recourse, 
+		I may contact my financial institution or visit www.cdnpay.ca. The financial institution shall reimburse me, on behalf of the organization, for any 
+		amounts withdrawn in error, within 90 calendar days of the withdrawal, provided that the reimbursement is claimed for a valid reason. I understand 
+		that a claim to this effect must be made to my financial institution following the procedure it will provide for that purpose. Finally, I acknowledge 
+		that a claim for reimbursement filed after the aforementioned time limits must be settled between me and PAC, without any liability or commitment on the 
+		part of my financial institution.  
+		</p>
+		<h4>CONSENT TO DISCLOSURE OF INFORMATION</h4>
+		<p>
+		I hereby consent to the disclosure of the information contained in my pre- authorized debit enrolment agreement to the 
+		financial institution, provided such information is directly related to and required for the smooth application of the rules governing pre-authorized debits.  
+		</p>
