8000349e2c4cf02995b92acc571b9c1df1ad2470
Dan Davies Brackett <dan@daviesbrackett.com>
2014-08-27 20:15:58 -0700

    another random todo for stuff that doesn't work yet anyway

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,7 @@ Route::post('/login', 'HomeController@postLogin');
 Route::get('/logout', 'HomeController@getLogout');
 
 Route::get('/account', array('before' => 'auth', 'uses' => 'OrderController@getAccount'));
-Route::post('/account', array('before' => 'csrf', 'uses' =>'OrderController@postAccount'));
+Route::post('/account', array('before' => 'csrf'/*auth too*/, 'uses' =>'OrderController@postAccount'));
 Route::get('/new', 'OrderController@getNew');
 Route::post('/new', array('before' => 'csrf', 'uses' =>'OrderController@postNew'));
 
