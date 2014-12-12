8dde2b50ae3b2fa594ad0663e16a64056aff0711
Dan Davies Brackett <dan@daviesbrackett.com>
2014-12-11 21:47:02 -0800

    this never worked

--- app/views/tracking/bucket.blade.php
+++ app/views/tracking/bucket.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="masthead">
 	<h1>{{{money_format( '$%n', $sum)}}} raised for {{{$name}}} so far</h1>
-	<h3>Based on last month's orders ({{{$pastSupporters}}} supporters), you'll have raised {{{money_format( '$%n', $projection )}}} by the end of the school year.</h3>
+	<h3>Based on last month's orders, you'll have raised {{{money_format( '$%n', $projection )}}} by the end of the school year.</h3>
 	<h3>{{{$name}}} currently has {{{$currentSupporters}}} supporters.</h3>
 </div>
 <div class="container-fluid">
