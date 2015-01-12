39d4e3f505f2f3e0dd591df4e885df1e3219be16
Dan Davies Brackett <dan@daviesbrackett.com>
2015-01-12 13:09:37 -0800

    attempt all payments even when some fail

--- app/components/eventHandlers/CreditOrderHandler.php
+++ app/components/eventHandlers/CreditOrderHandler.php
@@ -1,6 +1,7 @@
 <?php namespace NWSCards\components\eventHandlers;
 use \CutoffDate;
 use Stripe_Charge;
+use Stripe_CardError;
 class CreditOrderHandler {
 	public function handle($date) {
 		\Stripe::setApiKey($_ENV['stripe_secret_key']);
@@ -13,14 +14,23 @@ class CreditOrderHandler {
 		foreach($cutoff->orders as $order)
 		{
 			$cards = $order->saveon_onetime + $order->coop_onetime + $order->saveon + $order->coop;
-			if($order->isCreditcard() && $cards != 0 )
+			if($order->isCreditcard() && $cards != 0 && !$order->paid)
 			{
-				Stripe_Charge::create([
-					'customer' => $order->user->stripe_id,
-					'amount' => $cards * 100 * 100, //amount is in cents
-					'currency' => 'cad',
-					'description' => 'grocery card order for '.$cutoff->deliverydate(),
-					]);
+				try
+				{
+					Stripe_Charge::create([
+						'customer' => $order->user->stripe_id,
+						'amount' => $cards * 100 * 100, //amount is in cents
+						'currency' => 'cad',
+						'description' => 'grocery card order for '.$cutoff->deliverydate(),
+						]);
+				}
+				catch(Stripe_CardError $ex) 
+				{
+
+				}
+				$order->paid = true;
+				$order->save();
 			}
 		}
 		return "CC orders charged for " . $date;
