5ee50ca5e0ffc604add5be593bf7387f8c7a6088
Dan Davies Brackett <dan@daviesbrackett.com>
2015-01-17 20:33:11 -0800

    lipstick

--- app/views/admin/newsale.blade.php
+++ app/views/admin/newsale.blade.php
@@ -20,6 +20,7 @@
 		<div class="form-group">
 			<label for="user">Buyer Email</label>
 			{{ Form::input('text', 'user', null, ['class' => 'form-control', 'placeholder' => 'must already be a user']) }}
+			TODO: search function here by name, showing email address
 		</div>
 		<div class="form-group">
 			<label for="coop">Coop cards</label>
@@ -35,77 +36,84 @@
 				<span class="input-group-addon">x $100</span>
 			</div>
 		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('marigold') }}
-					Marigold Daycare
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('daisy') }}
-					Daisy Daycare
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('sunflower') }}
-					Sunflower Kindergarten
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('bluebell') }}
-					Bluebell Kindergarten
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_1') }}
-					Class 1
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_2') }}
-					Class 2
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_3') }}
-					Class 3
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_4') }}
-					Class 4
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_5') }}
-					Class 5
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_6') }}
-					Class 6
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_7') }}
-					Class 7
-				</label></div>
-		</div>
-		<div class="form-group">
-			<div class="checkbox"><label>
-					{{ Form::checkbox('class_8') }}
-					Class 8
-				</label></div>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('marigold') }}
+							Marigold Daycare
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('daisy') }}
+							Daisy Daycare
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('sunflower') }}
+							Sunflower Kindergarten
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('bluebell') }}
+							Bluebell Kindergarten
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_1') }}
+							Class 1
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_2') }}
+							Class 2
+						</label></div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_3') }}
+							Class 3
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_4') }}
+							Class 4
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_5') }}
+							Class 5
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_6') }}
+							Class 6
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_7') }}
+							Class 7
+						</label></div>
+				</div>
+				<div class="form-group">
+					<div class="checkbox"><label>
+							{{ Form::checkbox('class_8') }}
+							Class 8
+						</label></div>
+				</div>
+			</div>
 		</div>
 	{{Form::close()}}
 </div>
