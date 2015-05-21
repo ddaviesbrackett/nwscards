f8c997519d0d8788f76464e5fb38a3be3cc9a23c
Dan Davies Brackett <dan@daviesbrackett.com>
2015-05-20 23:45:02 -0700

    TEMPORARY CHANGE to deadline reminder for end of year

--- app/views/emails/deadlinereminder.blade.php
+++ app/views/emails/deadlinereminder.blade.php
@@ -3,16 +3,20 @@
 	<h2>Hi {{{$user->name}}},</h2>
 	
 	<p>
-		Need to change your grocery card order? You have until tomorrow, {{{$cutoff->cutoffdate()->format('l, F jS')}}}, at midnight. 
-		@if($user->saveon > 0 || $user->coop > 0 || $user->saveon_onetime > 0 || $user->coop_onetime > 0)
-			If you do not change your order by then, your account will be debited or your credit card will be charged the amount of your order 
-			on {{{$cutoff->chargedate()->format('l, F jS')}}}. 
-		@endif
+		Summer holidays are fast approaching - and with it the last grocery card order of the year. 
+		<b><a href="https://grocerycards.nelsonwaldorf.org/edit">Order extra cards</a> before midnight on Tuesday, May 26th to make sure you have plenty for the summer.</b>
 	</p>
 	<p>
-		You can change your regular order OR order extra cards <b>just once</b> 
-		at <a href="https://grocerycards.nelsonwaldorf.org/edit">https://grocerycards.nelsonwaldorf.org/edit</a>. Heck, you could even do both!
-		Log in with this email address and the password you signed up with. (Forgot your password? You can reset it at the login screen.) 
+	The Grocery Card Fairies are taking the summer off, but we'll be back in September. All regular orders will be suspended for the summer. 
+	We'll let you know when you can reactivate so you don't miss an order.
+	</p>
+	<p>
+		<b><a href="https://grocerycards.nelsonwaldorf.org/account/extracards">Add extra cards</a> to your order - 
+		or <a href="https://grocerycards.nelsonwaldorf.org/edit">make a one-time order</a>.</b> 
+		Just click the link - it's easy! (If you forget your password, you can reset it <a href="https://grocerycards.nelsonwaldorf.org/password/remind">here</a>.)
+	</p>
+	<p>
+	Together we've <b>raised over $35,000</b> this school year. <b>Let's get to $40,000!</b>
 	</p>
 	@if($user->saveon > 0 || $user->coop > 0)
 		<p>
@@ -49,7 +53,7 @@
 	@endif
 	<p>
 		Thank you for your support,<br/>
-		Donna Davies Brackett<br/>
+		Snow Colbeck<br/>
 	<p>NWS Grocery Cards Committee</p>
 	</p>
 </body>
