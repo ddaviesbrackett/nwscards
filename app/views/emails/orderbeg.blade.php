cdaeb8ae1b8664fab17a4bf8e9700bfa89244b9f
Dan Davies Brackett <dan@daviesbrackett.com>
2015-05-23 12:43:19 -0700

    Revert "TEMPORARY CHANGE to deadline reminder handler for end of year"
    This reverts commit ee1d91cac782b8e65ae940c92007181de96e5bcc.

--- app/views/emails/orderbeg.blade.php
+++ app/views/emails/orderbeg.blade.php
@@ -2,20 +2,12 @@
 <body>
 	<h2>Hi {{{$user->name}}},</h2>
 	<p>
-		Summer holidays are fast approaching - and with it the last grocery card order of the year. 
-		<b><a href="https://grocerycards.nelsonwaldorf.org/edit">Order extra cards</a> before midnight on Tuesday, May 26th to make sure you have plenty for the summer.</b>
+		You're not currently ordering grocery cards. If you'd like more cards, please order them by
+		tomorrow, {{{$cutoff->cutoffdate()->format('l, F jS')}}}, at midnight. You can get cards just once, or sign up for a regular order.
 	</p>
 	<p>
-	The Grocery Card Fairies are taking the summer off, but we'll be back in September. All regular orders will be suspended for the summer. 
-	We'll let you know when you can reactivate so you don't miss an order.
-	</p>
-	<p>
-		<b><a href="https://grocerycards.nelsonwaldorf.org/account/extracards">Add extra cards</a> to your order - 
-		or <a href="https://grocerycards.nelsonwaldorf.org/edit">make a one-time order</a>.</b> 
-		Just click the link - it's easy! (If you forget your password, you can reset it <a href="https://grocerycards.nelsonwaldorf.org/password/remind">here</a>.)
-	</p>
-	<p>
-	Together we've <b>raised over $35,000</b> this school year. <b>Let's get to $40,000!</b>
+		Order cards at <a href="https://grocerycards.nelsonwaldorf.org/edit">https://grocerycards.nelsonwaldorf.org/edit</a>.
+		Log in with this email address and the password you signed up with. (Forgot your password? You can reset it at the login screen.) 
 	</p>
 	<p>
 		Thank you for your support,<br/>
