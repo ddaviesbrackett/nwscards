124f8ae1d1ba1b54c275f16dbe1955bb22fe93d4
Dan Davies Brackett <dan@daviesbrackett.com>
2015-08-29 19:10:26 +0100

    wording tweaks to special email

--- app/views/emails/special-september-resume.blade.php
+++ app/views/emails/special-september-resume.blade.php
@@ -2,18 +2,18 @@
 <body>
 	<h2>Hi {{{$user->name}}},</h2>
 
-<p><b>The Grocery Cards Fairies are back and we are ready to take your orders for September!</b>
+<p><b>The Grocery Cards Fairies are back and we are ready to take your orders for September!</b></p>
 
-You can press this button to get your cards again:
+<p>The first order deadline is Sept 1st.  Your cards will be mailed out or ready for pick up at the NWS Wednesday Sept 9th between 8-8:30am or 2:30 - 3pm at 
+the bottom of the stairs.</p>
+
+<p>Press this button to get your cards again:
 <a style="display:block;margin:3em auto;padding:1em;text-align:center;max-width:25em;color:White;background-color:#a40016;border-radius:5px;" 
 href="https://grocerycards.nelsonwaldorf.org/email-resume?uid={{{$user->id}}}}&code={{{$user->reactivation_code}}}"><span style="font-size:2em;">Resume My Order</span></a>
 <p>You can also resume your order at <a href="https://grocerycards.nelsonwaldorf.org/Resume">https://grocerycards.nelsonwaldorf.org/Resume</a>.
 This will reactivate your account. You can then make changes or edit your order as needed. It is easy. </p>
 
 <p>(Forgot your password? Don't worry you can reset it when you log in.)</p>
-
-<p>The first order deadline is Sept 1st.  Your cards will be mailed out or ready for pick up at the NWS Wednesday Sept 9th between 8-8:30am or 2:30 - 3pm at 
-the bottom of the stairs.</p>
 
 <p>Last year we raised over $40,000 without spending any extra money! It is amazing what we can accomplish when we work together.  
 Resume your order now so we can get started for this year.</p>
