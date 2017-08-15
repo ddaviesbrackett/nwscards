94516b20a40bb7fcfeb506a1af0256807487d18a
yshaool <yshaool@gmail.com>
2017-08-15 09:31:53 -0700

    Changes to PAD for 2018

--- app/views/partial/debitterms.blade.php
+++ app/views/partial/debitterms.blade.php
@@ -1,21 +1,19 @@
 		<p>
-       	I authorize the Nelson Waldorf School Parent Association (hereinafter “PAC”) to make pre-authorized debits (PAD) from my account with the aforementioned 
-       	financial institution, either:
+       	I authorize the Nelson Waldorf School Parent Council (hereinafter “PC”) to make pre-authorized debits (PAD) from my account with my financial institution as detailed in my order on my account page @ grocerycards@nelsonwaldorf.org, either:
        	<ul>
 	       	<li>bi-weekly, commencing on {{{$dates['biweekly']['charge']}}} and ending on September 4, 2018</li>
 	       	<li>monthly, commencing on {{{$dates['monthly']['charge']}}} and ending on September 4, 2018</li>
        	</ul>
        as selected on the order form on my account page at grocerycards@nelsonwaldorf.org. 
-       <p>This is a variable PAD: For each withdrawal my account will be debited for the total amount on my order form (recurring order plus any one-time order) as ordered for each scheduled order at grocerycards@nelsonwaldorf.org. I agreed that PC may reduce the standard period of pre-notification for variable amount PADs. PC will send me notice of the amount of each PAD at least 4 calendar days before the PAD is due. </p>
+       <p>This is a variable PAD: For each withdrawal my account will be debited for the total amount on my order form (recurring order plus any one-time order) as ordered for each scheduled order at grocerycards@nelsonwaldorf.org. I agree that PC may reduce the standard period of pre-notification for variable amount PADs. PC will send me notice of the amount of each PAD at least 4 calendar days before the PAD is due.  </p>
        	<p>
-       	Each withdrawal will correspond to a fixed amount equal to the total stated on the order form.  
-       	I understand my account will be debited two business days before the published delivery dates for my order frequency. 
+       	Each withdrawal will correspond to a fixed amount equal to the total stated on the order form. I understand my account will be debited two calendar days before the published delivery dates for my order frequency. 
        	</p>
        	<p>Type of Pre-Authorized Debit: PERSONAL </p>
         
        	<h4>WAIVER</h4>  
        	<p>
-        I hereby waive any and all requirements for pre-notification of debiting. As a Written Confirmation of this PAD Agreement, including information on my account and my financial institution as detailed in my order on my account page at grocerycards@nelsonwaldorf.org, I will receive a copy of this Agreement by email at least 4 calendar days before the first PAD withdrawal and waive all other confirmation before the first payment.  
+        I hereby waive any and all requirements for pre-notification of debiting. As a Written Confirmation of this PAD Agreement, including information on my account and my financial institution as detailed in my order on my account page at grocerycards@nelsonwaldorf.org, I will receive a copy of this Agreement by email at least 4 calendar days before the first PAD withdrawal and waive all other confirmation before the first payment.   
        	</p>
            
        	<h4>SIGNATURES</h4>
@@ -23,33 +21,15 @@
 
 		<h4>CHANGE AND/OR CANCELLATION: </h4>
 
-		<p>I will advise PAC of any changes to this Agreement including, without limitation, changes to my account number, 
-		  by sending a written notice to PAC at grocerycards@nelsonwaldorf.org or by updating my order 
-		  at <a href="https://grocerycards.nelsonwaldorf.org/account">my account page</a> 
-		  at least 10 business days prior to the next withdrawal. 
-		I retain the right to revoke my authorization at any time, by sending a written notice to PAC at grocerycards@nelsonwaldorf.org or by updating my order 
-		at <a href="https://grocerycards.nelsonwaldorf.org/account">my account page</a>.
-		  at least 10 business days prior to the next withdrawal. To obtain a sample of the cancellation form or for more information on my right to cancel a PAD Agreement, 
-		I may contact my financial institution or consult the Canadian Payments Association at <a href="http://www.cdnpay.ca">www.cdnpay.ca</a>. 
-		I agree to release the financial institution of any liability if the revocation is not respected, except in the case of gross negligence on its part. 
-		I agree that the financial institution at which I maintain the account is not required to verify that the payment is debited in accordance with this authorization. 
-		  I also certify that every person whose signature is required for the operation of the aforementioned account has signed this authorization.  
-		I acknowledge that the delivery of this authorization to PAC constitutes delivery by me to the aforementioned financial institution.  
+		<p>I will advise PC of any changes to this Agreement including, without limitation, changes to my account number, by sending a written notice to PC at grocerycards@nelsonwaldorf.org or by updating my order at my account page at least 10 business days prior to the next withdrawal. I retain the right to revoke my authorization at any time, by sending a written notice to PC at grocerycards@nelsonwaldorf.org or by updating my order at my account page at least 10 business days prior to the next withdrawal. To obtain a sample of the cancellation form or for more information on my right to cancel a PAD Agreement, I may contact my financial institution or consult the Canadian Payments Association at www.payments.ca. I agree to release the financial institution of any liability if the revocation is not respected, except in the case of gross negligence on its part. I agree that the financial institution at which I maintain the account is not required to verify that the payment is debited in accordance with this authorization. I also certify that every person whose signature is required for the operation of my account has signed this authorization. I acknowledge that the delivery of this authorization to PC constitutes delivery by me to the aforementioned financial institution.   
 		</p>
 		<h4>REIMBURSEMENT</h4>
 		<p>                
-		I have certain rights of recourse if a debit does not comply with the terms of this Agreement. For example, I have the right to receive reimbursement 
-		for any PAD that is not authorized or that is not compatible with the terms of this PAD Agreement. For more information on my rights of recourse, 
-		I may contact my financial institution or visit www.cdnpay.ca. The financial institution shall reimburse me, on behalf of the organization, for any 
-		amounts withdrawn in error, within 90 calendar days of the withdrawal, provided that the reimbursement is claimed for a valid reason. I understand 
-		that a claim to this effect must be made to my financial institution following the procedure it will provide for that purpose. Finally, I acknowledge 
-		that a claim for reimbursement filed after the aforementioned time limits must be settled between me and PAC, without any liability or commitment on the 
-		part of my financial institution.  
+		I have certain rights of recourse if a debit does not comply with the terms of this Agreement. For example, I have the right to receive reimbursement for any PAD that is not authorized or that is not compatible with the terms of this PAD Agreement. To obtain a form for a Reimbursement Claim, or for more information on my rights of recourse, I may contact my financial institution or visit www.payments.ca. The financial institution shall reimburse me, on behalf of the organization, for any amounts withdrawn in error, within 90 calendar days of the withdrawal, provided that the reimbursement is claimed for a valid reason. I understand that a claim to this effect must be made to my financial institution following the procedure it will provide for that purpose. Finally, I acknowledge that a claim for reimbursement filed after the aforementioned time limits must be settled between me and PC, without any liability or commitment on the part of my financial institution.  
 		</p>
 		<h4>CONSENT TO DISCLOSURE OF INFORMATION</h4>
 		<p>
-		I hereby consent to the disclosure of the information contained in my pre- authorized debit enrolment agreement to the 
-		financial institution, provided such information is directly related to and required for the smooth application of the rules governing pre-authorized debits.  
+		I hereby consent to the disclosure of the information contained in my pre- authorized debit enrolment agreement to the financial institution, provided such information is directly related to and required for the smooth application of the rules governing pre-authorized debits.   
 		</p>
                 
                 
